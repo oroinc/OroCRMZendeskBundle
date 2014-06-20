8e383088010f9b5331da26f17b2eb67cd68cbe48
AlexandrDmitriev <admitriev@magecore.com>
2014-06-20 20:56:53 +0300

    CRM-1421: Demo changes

--- ImportExport/Strategy/Provider/OroEntityProvider.php
+++ ImportExport/Strategy/Provider/OroEntityProvider.php
@@ -4,7 +4,9 @@ namespace OroCRM\Bundle\ZendeskBundle\ImportExport\Strategy\Provider;
 
 use Doctrine\ORM\EntityManager;
 
+use Oro\Bundle\UserBundle\Entity\Email;
 use Oro\Bundle\UserBundle\Entity\User as OroUser;
+use OroCRM\Bundle\ContactBundle\Entity\ContactEmail;
 use OroCRM\Bundle\ZendeskBundle\Entity\User as ZendeskUser;
 use OroCRM\Bundle\ContactBundle\Entity\Contact;
 use OroCRM\Bundle\ContactBundle\Entity\ContactPhone;
@@ -41,6 +43,22 @@ class OroEntityProvider
         $oroUser = $this->entityManager->getRepository('OroUserBundle:User')
             ->findOneBy(array('email' => $user->getEmail()));
 
+        if (!$oroUser) {
+            /**
+             * @var Email $email
+             */
+            $email = $this->entityManager->getRepository('OroUserBundle:Email')
+                ->findOneBy(
+                    array(
+                        'email' => $user->getEmail()
+                    )
+                );
+
+            if ($email) {
+                $oroUser = $email->getUser();
+            }
+        }
+
         return $oroUser;
     }
 
@@ -58,29 +76,41 @@ class OroEntityProvider
      */
     public function getContact(ZendeskUser $user)
     {
-        $contact = $this->entityManager->getRepository('OroCRMContactBundle:Contact')
+        if (!$user->getEmail()) {
+            return null;
+        }
+        /**
+         * @var ContactEmail $contactEmail
+         */
+        $contactEmail = $this->entityManager->getRepository('OroCRMContactBundle:ContactEmail')
             ->findOneBy(
                 array(
                     'email' => $user->getEmail()
-                )
+                ),
+                array('primary' => 'DESC')
             );
 
-        if (!$contact) {
-            $contact = new Contact();
-
-            if ($user->getPhone()) {
-                $phone = new ContactPhone();
-                $phone->setPrimary(true);
-                $phone->setPhone($phone);
-                $contact->addPhone($phone);
-            }
-
-            $contact->setEmail($user->getEmail());
-
-            $nameParts = array_pad(explode(' ', $user->getName(), 2), 2, '');
-            $contact->setFirstName($nameParts[0]);
-            $contact->setLastName($nameParts[1]);
+        if ($contactEmail) {
+            return $contactEmail->getOwner();
         }
+
+        $contact = new Contact();
+
+        if ($user->getPhone()) {
+            $phone = new ContactPhone();
+            $phone->setPrimary(true);
+            $phone->setPhone($phone);
+            $contact->addPhone($phone);
+        }
+
+        $email = new ContactEmail();
+        $email->setPrimary(true);
+        $email->setEmail($user->getEmail());
+        $contact->addEmail($email);
+
+        $nameParts = array_pad(explode(' ', $user->getName(), 2), 2, '');
+        $contact->setFirstName($nameParts[0]);
+        $contact->setLastName($nameParts[1]);
 
         return $contact;
     }
