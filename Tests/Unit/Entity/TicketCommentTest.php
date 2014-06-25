ba47ef2c48a787a6a1a81657c989bf08984b1a3d
AlexandrDmitriev <admitriev@magecore.com>
2014-06-25 13:21:51 +0300

    CRM-1436: Add additional date times to Zendesk entities (User, Tickets, TicketComment)  - add tests

--- Tests/Unit/Entity/TicketCommentTest.php
+++ Tests/Unit/Entity/TicketCommentTest.php
@@ -28,6 +28,40 @@ class TicketCommentTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($value, $this->target->{'get' . $property}());
     }
 
+    public function testSetUpdatedAtLockedUpdateByLifeCycleCallback()
+    {
+        $expected = date_create_from_format('Y-m-d', '2012-10-10');
+        $this->target->setUpdatedAt($expected);
+        $this->target->preUpdate();
+        $this->assertSame($expected, $this->target->getUpdatedAt());
+    }
+
+    public function testPrePersist()
+    {
+        $this->assertNull($this->target->getCreatedAt());
+        $this->assertNull($this->target->getUpdatedAt());
+
+        $this->target->prePersist();
+
+        $this->assertInstanceOf('\DateTime', $this->target->getCreatedAt());
+        $this->assertInstanceOf('\DateTime', $this->target->getUpdatedAt());
+
+        $expectedCreated = $this->target->getCreatedAt();
+        $expectedUpdated = $this->target->getUpdatedAt();
+
+        $this->target->prePersist();
+
+        $this->assertSame($expectedCreated, $this->target->getCreatedAt());
+        $this->assertSame($expectedUpdated, $this->target->getUpdatedAt());
+    }
+
+    public function testPreUpdate()
+    {
+        $this->assertNull($this->target->getUpdatedAt());
+        $this->target->preUpdate();
+        $this->assertInstanceOf('\DateTime', $this->target->getUpdatedAt());
+    }
+
     /**
      * @return array
      */
@@ -51,6 +85,8 @@ class TicketCommentTest extends \PHPUnit_Framework_TestCase
             array('author', $zendeskUser),
             array('ticket', $ticket),
             array('createdAt', new \DateTime()),
+            array('originCreatedAt', new \DateTime()),
+            array('updatedAt', new \DateTime()),
             array('relatedComment', $comment)
         );
     }
--- Tests/Unit/Entity/TicketTest.php
+++ Tests/Unit/Entity/TicketTest.php
@@ -30,6 +30,40 @@ class TicketTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($value, $this->target->{'get' . $property}());
     }
 
+    public function testSetUpdatedAtLockedUpdateByLifeCycleCallback()
+    {
+        $expected = date_create_from_format('Y-m-d', '2012-10-10');
+        $this->target->setUpdatedAt($expected);
+        $this->target->preUpdate();
+        $this->assertSame($expected, $this->target->getUpdatedAt());
+    }
+
+    public function testPrePersist()
+    {
+        $this->assertNull($this->target->getCreatedAt());
+        $this->assertNull($this->target->getUpdatedAt());
+
+        $this->target->prePersist();
+
+        $this->assertInstanceOf('\DateTime', $this->target->getCreatedAt());
+        $this->assertInstanceOf('\DateTime', $this->target->getUpdatedAt());
+
+        $expectedCreated = $this->target->getCreatedAt();
+        $expectedUpdated = $this->target->getUpdatedAt();
+
+        $this->target->prePersist();
+
+        $this->assertSame($expectedCreated, $this->target->getCreatedAt());
+        $this->assertSame($expectedUpdated, $this->target->getUpdatedAt());
+    }
+
+    public function testPreUpdate()
+    {
+        $this->assertNull($this->target->getUpdatedAt());
+        $this->target->preUpdate();
+        $this->assertInstanceOf('\DateTime', $this->target->getUpdatedAt());
+    }
+
     /**
      * @return array
      */
@@ -67,6 +101,8 @@ class TicketTest extends \PHPUnit_Framework_TestCase
             array('priority', $ticketPriority),
             array('createdAt', new \DateTime()),
             array('updatedAt', new \DateTime()),
+            array('originCreatedAt', new \DateTime()),
+            array('originUpdatedAt', new \DateTime()),
             array('dueAt', new \DateTime()),
             array('requester', $zendeskUser),
             array('assignee', $zendeskUser),
