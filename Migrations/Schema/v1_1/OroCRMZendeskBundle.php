0a4555dd9bbc813af3caa1c6cbdb0962eee65769
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-08-01 20:56:47 +0300

    CRM-1610: Create migrations for incompatible initial migrations - zendesk

---
+++ Migrations/Schema/v1_1/OroCRMZendeskBundle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OroCRM\Bundle\ZendeskBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroCRMZendeskBundle implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $table = $schema->getTable('orocrm_zd_ticket');
+        $table->dropIndex('unq_origin_id_channel_id');
+        $table->addUniqueIndex(['origin_id', 'channel_id'], 'zd_ticket_oid_cid_unq');
+
+        $table = $schema->getTable('orocrm_zd_user');
+        $table->dropIndex('unq_origin_id_channel_id');
+        $table->addUniqueIndex(['origin_id', 'channel_id'], 'zd_user_oid_cid_unq');
+
+        $table = $schema->getTable('orocrm_zd_comment');
+        $table->dropIndex('unq_origin_id_channel_id');
+        $table->addUniqueIndex(['origin_id', 'channel_id'], 'zd_comment_oid_cid_unq');
+    }
+}
