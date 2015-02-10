414e52bd69d1a9d78583f9cde9ff5f1cd5f0110e
Valentine Velikiy <vvelykyi@magecore.com>
2015-02-10 12:29:14 +0200

    BAP-7447: Data migration to fix empty organization of contacts. - Prepared migration for large data.

--- Migrations/Data/ORM/SetOrganizationData.php
+++ Migrations/Data/ORM/SetOrganizationData.php
@@ -2,15 +2,17 @@
 
 namespace OroCRM\Bundle\ZendeskBundle\Migrations\Data\ORM;
 
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\QueryBuilder;
 
 use Oro\Bundle\TranslationBundle\DataFixtures\AbstractTranslatableEntityFixture;
 use OroCRM\Bundle\ContactBundle\Entity\Contact;
 
 class SetOrganizationData extends AbstractTranslatableEntityFixture
 {
+    const MAX_RESULTS = 1000;
+
     /**
      * Load entities to DB
      *
@@ -19,19 +21,31 @@ class SetOrganizationData extends AbstractTranslatableEntityFixture
     protected function loadEntities(ObjectManager $manager)
     {
         $repository = $manager->getRepository('OroOrganizationBundle:Organization');
-        $organization = $repository->getFirst();
+        $defaultOrganization = $repository->getFirst();
 
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = $manager->getRepository('OroCRMContactBundle:Contact')->createQueryBuilder('entity');
         $criteria = new Criteria();
         $criteria->where($criteria->expr()->isNull('organization'));
-        /** @var ArrayCollection $collection */
-        $collection = $manager->getRepository('OroCRMContactBundle:Contact')->matching($criteria);
+        $queryBuilder->addCriteria($criteria);
+        $queryBuilder->setFirstResult(0);
+        $queryBuilder->setMaxResults(self::MAX_RESULTS);
 
-        foreach ($collection as $item) {
-            /** @var Contact $item */
-            $item->setOrganization($organization);
-            $manager->persist($item);
+        $result = $queryBuilder->getQuery()->execute();
+        while(count($result)) {
+            foreach ($result as $entity) {
+                /** @var Contact $entity */
+                if ($entity->getOwner() && $entity->getOwner()->getOrganization()) {
+                    $organization = $entity->getOwner()->getOrganization();
+                } else {
+                    $organization = $defaultOrganization;
+                }
+                $entity->setOrganization($organization);
+                $manager->persist($entity);
+            }
+            $manager->flush();
+            $manager->clear();
+            $result = $queryBuilder->getQuery()->execute();
         }
-
-        $manager->flush();
     }
 }
