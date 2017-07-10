55a1767b1a5d90f17419dfcd4fc86124cae39280
Alexandr Parkhomenko <alexandr-parkhomenko@users.noreply.github.com>
2017-07-10 16:53:18 +0300

    CRM-8357: Zendesk integration crashes on duplicated Users (2.0) (#11562)
    - Added logger to SyncState
    - Fixed unit tests
    - Fixed code after code review
    - Added ExecutionContextTrait to decrease duplication in tests
    - Added checking on specific date format in ExecutionContextTrait
    - Update parameter name in ZendTransportInterface to make it consistensy with parameter in SyncState
    - Added const `SEARCH_TYPE` that indicates type that will be using in parameter 'query' of request
    - Added unit tests to test SyncState model

---
+++ Migrations/Schema/v1_2_2/AddLastSyncDateToChannelStatus.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Oro\Bundle\ZendeskBundle\Migrations\Schema\v1_2_2;
+
+use Psr\Log\LoggerInterface;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Connection;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\MigrationBundle\Migration\ParametrizedMigrationQuery;
+use Oro\Bundle\IntegrationBundle\Entity\Status;
+use Oro\Bundle\ZendeskBundle\Provider\ChannelType;
+use Oro\Bundle\ZendeskBundle\Provider\UserConnector;
+use Oro\Bundle\ZendeskBundle\Provider\TicketConnector;
+use Oro\Bundle\ZendeskBundle\Model\SyncState;
+
+class AddLastSyncDateToChannelStatus extends ParametrizedMigrationQuery implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $queries->addQuery(new self());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return "This migration update field 'data' " .
+            "in table 'oro_integration_channel_status' (this table keeps data about last sync with integration) " .
+            "with information about 'last sync datetime' " .
+            "for all records that relate to channel 'zendesk' and one of next connectors 'user' or 'ticket'";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(LoggerInterface $logger)
+    {
+        $selectStatusQuery = <<<SQL
+SELECT main.id, main.date, main.data FROM oro_integration_channel_status main WHERE main.id IN (
+SELECT MAX(s.id) AS id
+FROM oro_integration_channel_status s
+INNER JOIN oro_integration_channel ch ON ch.type = :channel_type AND s.channel_id = ch.id
+WHERE s.code = :completed_status_code AND s.connector IN (:connectors)
+GROUP BY s.channel_id, s.connector, s.code )
+SQL;
+        $selectStatusParams = [
+            'channel_type'          => ChannelType::TYPE,
+            'completed_status_code' => (string) Status::STATUS_COMPLETED,
+            'connectors'            => [UserConnector::TYPE, TicketConnector::TYPE]
+        ];
+
+        $selectStatusTypes = [
+            'channel_type'          => Type::STRING,
+            'completed_status_code' => Type::STRING,
+            'connectors'            => Connection::PARAM_STR_ARRAY
+        ];
+
+        $this->logQuery($logger, $selectStatusQuery, $selectStatusParams, $selectStatusTypes);
+        $result = $this->connection->fetchAll($selectStatusQuery, $selectStatusParams, $selectStatusTypes);
+
+        $jsonArrayType = Type::getType(Type::JSON_ARRAY);
+        $platform = $this->connection->getDatabasePlatform();
+
+        try {
+            $this->connection->beginTransaction();
+            foreach ($result as $statusRecord) {
+                $id = $statusRecord['id'];
+                $statusDateRaw = $statusRecord['date'];
+                $statusDataRaw = $statusRecord['data'];
+                $statusData = $jsonArrayType->convertToPHPValue($statusDataRaw, $platform);
+
+                if (!isset($statusData[SyncState::LAST_SYNC_DATE_KEY])) {
+                    $statusDate = new \DateTime($statusDateRaw, new \DateTimeZone('UTC'));
+                    $statusDate->setTime(0, 0, 0);
+                    $statusData[SyncState::LAST_SYNC_DATE_KEY] = $statusDate->format(\DateTime::ISO8601);
+                    $this->connection->update(
+                        'oro_integration_channel_status',
+                        ['data' => $statusData],
+                        ['id' => $id],
+                        [Type::JSON_ARRAY]
+                    );
+                }
+            }
+            $this->connection->commit();
+        } catch (\Exception $e) {
+            $this->connection->rollBack();
+            throw $e;
+        }
+    }
+}
---
+++ Tests/Unit/Model/SyncStateTest.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Oro\Bundle\ZendeskBundle\Tests\Unit\Model;
+
+use Psr\Log\LoggerInterface;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+use Oro\Bundle\IntegrationBundle\Entity\Channel;
+use Oro\Bundle\IntegrationBundle\Entity\Repository\ChannelRepository;
+use Oro\Bundle\IntegrationBundle\Entity\Status;
+use Oro\Bundle\ZendeskBundle\Model\SyncState;
+
+class SyncStateTest extends \PHPUnit_Framework_TestCase
+{
+    const STATUS_ID = 1;
+
+    /**
+     * @var SyncState
+     */
+    protected $syncState;
+
+    /**
+     * @var string
+     */
+    protected $connector = "CONNECTOR";
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $channel;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $channelRepository;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $logger;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $status;
+
+    protected function setUp()
+    {
+        $this->channel = $this->createMock(Channel::class);
+        $this->channelRepository = $this->createMock(ChannelRepository::class);
+        $managerRegistry = $this->createMock(ManagerRegistry::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $managerRegistry
+            ->expects($this->once())
+            ->method('getRepository')
+            ->with('OroIntegrationBundle:Channel')
+            ->willReturn($this->channelRepository);
+
+        $this->syncState = new SyncState($managerRegistry);
+        $this->syncState->setLogger($this->logger);
+    }
+
+    public function testLastStatusNotFound()
+    {
+        $this->channelRepository
+            ->expects($this->once())
+            ->method('getLastStatusForConnector')
+            ->with($this->channel, $this->connector, Status::STATUS_COMPLETED)
+            ->willReturn(null);
+
+        $this->assertSame(
+            null,
+            $this->syncState->getLastSyncDate($this->channel, $this->connector)
+        );
+    }
+
+    /**
+     * @param array $data
+     * @param $error
+     * @param $result
+     *
+     * @dataProvider getLastSyncDateProvider
+     */
+    public function testGetLastSyncDate(array $data, $error, $result)
+    {
+        $this->getMockForStatusEntity($data);
+
+        if (false !== $error) {
+            $this->logger
+                ->expects($this->once())
+                ->method('error')
+                ->with($error);
+        }
+
+        $this->assertEquals(
+            $result,
+            $this->syncState->getLastSyncDate($this->channel, $this->connector)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getLastSyncDateProvider()
+    {
+        return [
+            "Key 'lastSyncDate' isn't set" => [
+                'data' => [],
+                'error' => false,
+                'result' => null
+            ],
+            "Data by key 'lastSyncDate' has incorrect format" => [
+                'data' => [
+                    SyncState::LAST_SYNC_DATE_KEY => 'incorrect_format'
+                ],
+                'error' => 'Status with [id=1] contains incorrect date format in data by key "lastSyncDate".',
+                'result' => null
+            ],
+            "Data by key 'lastSyncDate' present and has correct format" => [
+                'data' => [
+                    SyncState::LAST_SYNC_DATE_KEY => '2004-02-12T15:19:21+00:00'
+                ],
+                'error' => false,
+                'result' => new \DateTime('2004-02-12T15:19:21+00:00')
+            ]
+        ];
+    }
+
+    /**
+     * @param array $data
+     */
+    protected function getMockForStatusEntity(array $data)
+    {
+        $this->status = $this->createMock(Status::class);
+
+        $this->status
+            ->expects($this->any())
+            ->method('getId')
+            ->willReturn(self::STATUS_ID);
+
+        $this->status
+            ->expects($this->atLeastOnce())
+            ->method('getData')
+            ->willReturn($data);
+
+        $this->channelRepository
+            ->expects($this->once())
+            ->method('getLastStatusForConnector')
+            ->with($this->channel, $this->connector, Status::STATUS_COMPLETED)
+            ->willReturn($this->status);
+    }
+
+    protected function tearDown()
+    {
+        unset(
+            $this->syncState,
+            $this->channel,
+            $this->logger,
+            $this->status,
+            $this->channelRepository
+        );
+    }
+}
