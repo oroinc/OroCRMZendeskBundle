601a5adb4951699969a60a92f4553c339670f382
Vova Soroka <vsoroka@oroinc.com>
2021-06-19 23:30:52 +0300

    BAP-20474: Remove setMethods() usage in tests for validators (#30132)

--- Tests/Unit/Provider/AbstractZendeskConnectorTest.php
+++ Tests/Unit/Provider/AbstractZendeskConnectorTest.php
@@ -2,89 +2,70 @@
 
 namespace Oro\Bundle\ZendeskBundle\Tests\Unit\Provider;
 
+use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
+use Oro\Bundle\ImportExportBundle\Context\ContextInterface;
+use Oro\Bundle\ImportExportBundle\Context\ContextRegistry;
+use Oro\Bundle\IntegrationBundle\Entity\Channel;
+use Oro\Bundle\IntegrationBundle\Entity\Transport;
+use Oro\Bundle\IntegrationBundle\Logger\LoggerStrategy;
+use Oro\Bundle\IntegrationBundle\Provider\ConnectorContextMediator;
+use Oro\Bundle\IntegrationBundle\Provider\TransportInterface;
+use Oro\Bundle\ZendeskBundle\Model\SyncState;
 use Oro\Bundle\ZendeskBundle\Provider\AbstractZendeskConnector;
+use Oro\Bundle\ZendeskBundle\Provider\Transport\ZendeskTransportInterface;
 
 class AbstractZendeskConnectorTest extends \PHPUnit\Framework\TestCase
 {
     use ExecutionContextTrait;
-    /**
-     * @var AbstractZendeskConnector
-     */
-    protected $connector;
 
-    /**
-     * @var \PHPUnit\Framework\MockObject\MockObject
-     */
-    protected $stepExecutor;
+    /** @var AbstractZendeskConnector */
+    private $connector;
 
-    /**
-     * @var \PHPUnit\Framework\MockObject\MockObject
-     */
-    protected $mediator;
+    /** @var StepExecution|\PHPUnit\Framework\MockObject\MockObject */
+    private $stepExecutor;
+
+    /** @var ConnectorContextMediator|\PHPUnit\Framework\MockObject\MockObject */
+    private $mediator;
 
     protected function setUp(): void
     {
-        $registry = $this->getMockBuilder('Oro\Bundle\ImportExportBundle\Context\ContextRegistry')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $logger = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Logger\LoggerStrategy')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->mediator = $this->getMockBuilder('Oro\Bundle\IntegrationBundle\Provider\ConnectorContextMediator')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $syncState = $this->getMockBuilder('Oro\Bundle\ZendeskBundle\Model\SyncState')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $context = $this->createMock('Oro\Bundle\ImportExportBundle\Context\ContextInterface');
-        $channel = $this->createMock('Oro\Bundle\IntegrationBundle\Entity\Channel');
-        $transportEntity = $this->createMock('Oro\Bundle\IntegrationBundle\Entity\Transport');
+        $registry = $this->createMock(ContextRegistry::class);
+        $logger = $this->createMock(LoggerStrategy::class);
+        $this->mediator = $this->createMock(ConnectorContextMediator::class);
+        $syncState = $this->createMock(SyncState::class);
+        $context = $this->createMock(ContextInterface::class);
+        $channel = $this->createMock(Channel::class);
+        $transportEntity = $this->createMock(Transport::class);
         $channel->expects($this->any())
             ->method('getTransport')
-            ->will($this->returnValue($transportEntity));
+            ->willReturn($transportEntity);
         $this->mediator->expects($this->any())
             ->method('getChannel')
-            ->will($this->returnValue($channel));
+            ->willReturn($channel);
         $registry->expects($this->any())
             ->method('getByStepExecution')
-            ->will($this->returnValue($context));
-        $constructorArgs = array(
+            ->willReturn($context);
+        $constructorArgs = [
             $syncState,
             $registry,
             $logger,
             $this->mediator
-        );
+        ];
 
-        $this->connector = $this->getMockBuilder('Oro\Bundle\ZendeskBundle\Provider\AbstractZendeskConnector')
+        $this->connector = $this->getMockBuilder(AbstractZendeskConnector::class)
             ->setConstructorArgs($constructorArgs)
-            ->setMethods(array('getConnectorSource'))
+            ->onlyMethods(['getConnectorSource'])
             ->getMockForAbstractClass();
-        $this->stepExecutor = $this->getMockBuilder('Akeneo\Bundle\BatchBundle\Entity\StepExecution')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->stepExecutor = $this->createMock(StepExecution::class);
 
-        $this->stepExecutor
-            ->expects($this->any())
+        $this->stepExecutor->expects($this->any())
             ->method('getExecutionContext')
             ->willReturn($this->initExecutionContext());
 
-        $iterator = $this->createMock('\Iterator');
+        $iterator = $this->createMock(\Iterator::class);
         $this->connector->expects($this->any())
             ->method('getConnectorSource')
-            ->will($this->returnValue($iterator));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function tearDown(): void
-    {
-        unset(
-            $this->connector,
-            $this->stepExecutor,
-            $this->mediator,
-            $this->executionContext
-        );
+            ->willReturn($iterator);
     }
 
     public function testValidateConfigurationThrowExceptionIfTransportInvalid()
@@ -92,25 +73,25 @@ class AbstractZendeskConnectorTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Option "transport" should implement "ZendeskTransportInterface"');
 
-        $transport = $this->createMock('Oro\Bundle\IntegrationBundle\Provider\TransportInterface');
+        $transport = $this->createMock(TransportInterface::class);
         $this->mediator->expects($this->any())
             ->method('getTransport')
-            ->will($this->returnValue($transport));
+            ->willReturn($transport);
         $this->connector->setStepExecution($this->stepExecutor);
     }
 
     public function testValidateConfiguration()
     {
-        $transport = $this->createMock('Oro\Bundle\ZendeskBundle\Provider\Transport\ZendeskTransportInterface');
+        $transport = $this->createMock(ZendeskTransportInterface::class);
         $this->mediator->expects($this->any())
             ->method('getTransport')
-            ->will($this->returnValue($transport));
+            ->willReturn($transport);
 
         $isUpdatedLastSyncDateCallback = $this->getIsUpdatedLastSyncDateCallback();
         $this->connector->setStepExecution($this->stepExecutor);
         $this->assertTrue(
             $isUpdatedLastSyncDateCallback(),
-            "Last sync date should be saved to context data !"
+            'Last sync date should be saved to context data !'
         );
     }
 }
