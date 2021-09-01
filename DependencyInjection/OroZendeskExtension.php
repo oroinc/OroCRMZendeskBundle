bdfca265feb0fa19ed6ea926de56b6dd3b23940d
Eugene Chetverikov <EugeneC@users.noreply.github.com>
2021-09-01 19:46:43 +0300

    BAP-20777: Upgrade symfony/dependency-injection and symfony/config to 5.3 (#30594)
    - upgraded symfony/dependency-injection to 5.3
    - upgraded symfony/config to 5.3
    - updated dev.locks
    
    | Production Changes                   | From      | To        | Compare                                                                                      |
    |--------------------------------------|-----------|-----------|----------------------------------------------------------------------------------------------|
    | composer/composer                    | 2.1.5     | 2.1.6     | [...](https://github.com/composer/composer/compare/2.1.5...2.1.6)                            |
    | composer/package-versions-deprecated | 1.11.99.2 | 1.11.99.3 | [...](https://github.com/composer/package-versions-deprecated/compare/1.11.99.2...1.11.99.3) |
    | jms/metadata                         | 2.5.0     | 2.5.1     | [...](https://github.com/schmittjoh/metadata/compare/2.5.0...2.5.1)                          |
    | microsoft/microsoft-graph            | 1.36.0    | 1.37.0    | [...](https://github.com/microsoftgraph/msgraph-sdk-php/compare/1.36.0...1.37.0)             |
    | seld/phar-utils                      | 1.1.1     | 1.1.2     | [...](https://github.com/Seldaek/phar-utils/compare/1.1.1...1.1.2)                           |
    | symfony/config                       | v4.4.27   | v5.3.4    | [...](https://github.com/symfony/config/compare/v4.4.27...v5.3.4)                            |
    | symfony/dependency-injection         | v4.4.27   | v5.3.4    | [...](https://github.com/symfony/dependency-injection/compare/v4.4.27...v5.3.4)              |
    
    - symfony/config upgrade: fixed symfony/config incompatability issues
    - symfony/config upgrade: removed unused configuration classes
    - symfony/config upgrade: updated docs
    - symfony/dependency-injection upgrade: replaced !tagged with !tagged_iterator
    - symfony/dependency-injection upgrade: moved from ResettableContainerInterface to ResetInterface
    - symfony/dependency-injection upgrade: removed InverseNamedTaggedServiceCompilerPass, InverseTaggedIteratorCompilerPass, PriorityTaggedServiceViaAddMethodCompilerPass, TaggedServicesCompilerPassTrait
    - symfony/dependency-injection upgrade: priority of some services was updated (inversed)
    - symfony/dependency-injection upgrade: fixed type inconsistency in usages of Psr\Container\ContainerInterface
    - fixed unit tests

--- DependencyInjection/OroZendeskExtension.php
+++ DependencyInjection/OroZendeskExtension.php
@@ -7,6 +7,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
+/**
+ * This is the class that loads and manages ZendeskBundle service configuration
+ */
 class OroZendeskExtension extends Extension
 {
     /**
@@ -14,10 +17,6 @@ class OroZendeskExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
         $loader->load('map.yml');
