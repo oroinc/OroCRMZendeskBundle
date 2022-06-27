579444ccdd821ef30ff0f8dd873a6cee471c9688
ikhrysieva <103260327+ikhrysieva@users.noreply.github.com>
2022-06-27 11:40:10 +0200

    CRM-9382: Can't install demo data without dev dependencies (#33052)

--- Migrations/Data/Demo/ORM/LoadTransportData.php
+++ Migrations/Data/Demo/ORM/LoadTransportData.php
@@ -2,33 +2,40 @@
 
 namespace Oro\Bundle\ZendeskBundle\Migrations\Data\Demo\ORM;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Persistence\ObjectManager;
 use Oro\Bundle\ZendeskBundle\Entity\ZendeskRestTransport;
-use Oro\Bundle\ZendeskBundle\Tests\Functional\DataFixtures\AbstractZendeskFixture;
 
-class LoadTransportData extends AbstractZendeskFixture
+/**
+ * Load demo data settings for zendesk transport.
+ */
+class LoadTransportData extends AbstractFixture
 {
-    protected $transportData = array(
-        array(
+    protected $transportData = [
+        [
             'url' => 'https://demo.zendesk.com',
             'email' => 'demo@mail.com',
             'token' => 'c8541140-fdfe-11e3-a3ac-0800200c9a66',
             'zendeskUserEmail' => 'demo_user@mail.com',
             'reference' => 'oro_zendesk:zendesk_demo_transport'
-        )
-    );
+        ]
+    ];
+
     /**
      * {@inheritdoc}
      */
     public function load(ObjectManager $manager)
     {
-        foreach ($this->transportData as $data) {
-            $transport = new ZendeskRestTransport();
+        $transport = new ZendeskRestTransport();
 
-            $this->setEntityPropertyValues($transport, $data, array('reference'));
-            $manager->persist($transport);
-            $this->setReference($data['reference'], $transport);
-        }
+        $data = reset($this->transportData);
+        $transport->setUrl($data['url']);
+        $transport->setEmail($data['email']);
+        $transport->setToken($data['token']);
+        $transport->setZendeskUserEmail($data['zendeskUserEmail']);
+
+        $manager->persist($transport);
+        $this->setReference('oro_zendesk:zendesk_demo_transport', $transport);
 
         $manager->flush();
     }
